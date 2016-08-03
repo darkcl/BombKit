3b8c458524ddaa947d5cc54a6f65b493d720316c
Yeung Yiu Hung <yeungyiuhung@cherrypicks.com>
2016-08-03 11:40:59 +0800

    Carthage Update

--- Carthage/Build/iOS/BombKit.framework/Headers/BombKit.h
+++ Carthage/Build/iOS/BombKit.framework/Headers/BombKit.h
@@ -16,4 +16,8 @@ FOUNDATION_EXPORT const unsigned char BombKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <BombKit/PublicHeader.h>
 
+#import <BombKit/BombKitConstant.h>
+
+#import <BombKit/BombKitModel.h>
 #import <BombKit/BombRequest.h>
+
